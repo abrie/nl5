b3bf877ba3eaf4b6f1879942df922b6fc85e06cd
abrie <abrie@hello>
2024-10-24 20:39:49 -0400

    Change the HELLO WORLD

--- src/App.ts
+++ src/App.ts
@@ -1,11 +1,11 @@
 class App {
-  mount(element: HTMLElement | null) {
-    if (element) {
-      const h1 = document.createElement('h1');
-      h1.textContent = 'Hello, World!';
-      element.appendChild(h1);
-    }
-  }
+	mount(element: HTMLElement | null) {
+		if (element) {
+			const h1 = document.createElement("div");
+			h1.textContent = "hello";
+			element.appendChild(h1);
+		}
+	}
 }
 
 export { App };
